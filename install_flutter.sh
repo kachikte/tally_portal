bc35a43ba68e480536fd37c8b07d5c5ed62620ab
Onochie-okeke Onyekachi <kachitheengineer@gmail.com>
2025-01-16 08:57:12 +0100

    Add Flutter installation script

--- install_flutter.sh
+++ install_flutter.sh
@@ -7,6 +7,12 @@ FLUTTER_VERSION="stable"
 echo "Installing Flutter..."
 git clone https://github.com/flutter/flutter.git -b $FLUTTER_VERSION
 export PATH="$PATH:$(pwd)/flutter/bin"
+echo "Added Flutter to PATH: $PATH"
+
+# Verify Flutter installation
+echo "Verifying Flutter installation..."
+which flutter
+flutter --version || { echo "Flutter installation failed"; exit 1; }
 
 # Install dependencies
 echo "Installing dependencies..."
@@ -18,5 +24,5 @@ echo "Configuring Flutter..."
 export CHROME_EXECUTABLE=/usr/bin/google-chrome
 flutter config --android-sdk /path/to/android-sdk
 
-# Verify Flutter installation
+# Verify Flutter setup
 flutter doctor
