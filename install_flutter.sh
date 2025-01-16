f19237e0dd79db1e68a95656987b32fe2034a698
Onochie-okeke Onyekachi <kachitheengineer@gmail.com>
2025-01-16 08:51:11 +0100

    Add Flutter installation script

--- install_flutter.sh
+++ install_flutter.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 
 # Set Flutter version
-FLUTTER_VERSION="3.24.5"  # Change to the desired version
+FLUTTER_VERSION="stable"
 
 # Install Flutter
 echo "Installing Flutter..."
 git clone https://github.com/flutter/flutter.git -b $FLUTTER_VERSION
 export PATH="$PATH:$(pwd)/flutter/bin"
 
+# Install dependencies
 echo "Installing dependencies..."
 sudo apt-get update
-sudo apt-get install clang libgtk-3-dev google-chrome-stable -y
+sudo apt-get install -y clang libgtk-3-dev google-chrome-stable android-sdk
 
-# Verify installation
+# Configure Flutter
+echo "Configuring Flutter..."
+export CHROME_EXECUTABLE=/usr/bin/google-chrome
+flutter config --android-sdk /path/to/android-sdk
+
+# Verify Flutter installation
 flutter doctor
